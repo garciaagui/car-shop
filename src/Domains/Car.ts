dabc89109707d7f89e1c2b747d4313db27e68273
Guilherme Garcia <garciaguig@gmail.com>
2023-02-13 20:07:40 -0300

    :sparkles: feat: Adiciona o Domain Car

---
+++ src/Domains/Car.ts
@@ -0,0 +1,96 @@
+class Car {
+  protected id: string | undefined;
+  protected model: string;
+  protected year: number;
+  protected color: string;
+  protected status: boolean;
+  protected buyValue: number;
+  private doorsQty: number;
+  private seatsQty: number;
+
+  constructor(
+    id: string | undefined,
+    model: string,
+    year: number,
+    color: string,
+    status = false,
+    buyValue: number,
+    doorsQty: number,
+    seatsQty: number,
+  ) {
+    this.id = id;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.buyValue = buyValue;
+    this.doorsQty = doorsQty;
+    this.seatsQty = seatsQty;
+    this.status = status;
+  }
+
+  public setId(id: string) {
+    this.id = id;
+  }
+
+  public getId() {
+    return this.id;
+  }
+
+  public setModel(model: string) {
+    this.model = model;
+  }
+
+  public getModel() {
+    return this.model;
+  }
+
+  public setYear(year: number) {
+    this.year = year;
+  }
+
+  public getYear() {
+    return this.year;
+  }
+
+  public setColor(color: string) {
+    this.color = color;
+  }
+
+  public getColor() {
+    return this.color;
+  }
+
+  public setStatus(status: boolean) {
+    this.status = status;
+  }
+
+  public getStatus() {
+    return this.status;
+  }
+
+  public setBuyValue(buyValue: number) {
+    this.buyValue = buyValue;
+  }
+
+  public getBuyValue() {
+    return this.buyValue;
+  }
+
+  public setDoorsQty(doorsQty: number) {
+    this.doorsQty = doorsQty;
+  }
+
+  public getDoorsQty() {
+    return this.doorsQty;
+  }
+
+  public setSeatsQty(seatsQty: number) {
+    this.seatsQty = seatsQty;
+  }
+
+  public getSeatsQty() {
+    return this.seatsQty;
+  }
+}
+
+export default Car;
