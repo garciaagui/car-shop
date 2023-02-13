3c135c99717046feea82bcd86ef4469d6964bd29
Guilherme Garcia <garciaguig@gmail.com>
2023-02-13 20:39:44 -0300

    :sparkles: feat: Adiciona Service de Car

---
+++ src/Services/CarService.ts
@@ -0,0 +1,20 @@
+import Car from '../Domains/Car';
+import ICar from '../Interfaces/ICar';
+import CarODM from '../Models/CarODM';
+
+class CarService {
+  private createCarDomain(car: ICar | null): Car | null {
+    if (car) {
+      return new Car({ ...car });
+    }
+    return null;
+  }
+
+  public async create(car: ICar) {
+    const carODM = new CarODM();
+    const newCar = await carODM.create(car);
+    return this.createCarDomain(newCar);
+  }
+}
+
+export default CarService;
