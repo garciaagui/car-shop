ebdf2ba45ffaaabc48ba5fcd690e11e11bf1e9a8
Guilherme Garcia <garciaguig@gmail.com>
2023-02-13 20:40:37 -0300

    :sparkles: feat: Adiciona Controller de Car

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import ICar from '../Interfaces/ICar';
+import CarService from '../Services/CarService';
+
+class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: CarService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new CarService();
+  }
+
+  public async create() {
+    const car: ICar = { ...this.req.body };
+
+    try {
+      const newCar = await this.service.create(car);
+      return this.res.status(201).json(newCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+
+export default CarController;
