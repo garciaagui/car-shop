87e86912c5c6ef229e915102af957af779b44acd
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:42:50 -0300

    :wrench: chore: Cria o arquivo de rotas /motorcycles

---
+++ src/Routes/MotorcycleRoutes.ts
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import MotorcycleController from '../Controllers/MotorcycleController';
+
+const routes = Router();
+
+export default routes;
--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import CarRoutes from './Routes/CarRoutes';
+import MotorcycleRoutes from './Routes/MotorcycleRoutes';
 import errorMiddleware from './Middlewares/errorMiddleware';
 
 const app = express();
 app.use(express.json());
 app.use('/cars', CarRoutes);
+app.use('/motorcycles', MotorcycleRoutes);
 app.use(errorMiddleware);
 
 export default app;
