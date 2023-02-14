ff69f18803b559be788e492c4ae072966b2e0681
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:08:35 -0300

    :test_tube: test: Adiciona o teste para o cenário de cadastro de novas motos

---
+++ tests/unit/Services/creatMotorcycle.test.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import { Model } from 'mongoose';
+
+describe('Testes de unidade de cadastro do Service de Motorcycle', function () {
+  afterEach(function () {
+    sinon.restore();
+  });
+  
+  it('Cadastra uma nova mota com SUCESSO', async function () {
+    // Arrange
+    const input: IMotorcycle = {
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30990,
+      category: 'Street',
+      engineCapacity: 600,
+    };
+
+    const output: Motorcycle = new Motorcycle({
+      id: '63319d80feb9f483ee823ac5',
+      model: 'Honda Cb 600f Hornet',
+      year: 2005,
+      color: 'Yellow',
+      status: true,
+      buyValue: 30990,
+      category: 'Street',
+      engineCapacity: 600,
+    });
+
+    sinon.stub(Model, 'create').resolves(output);
+
+    // Act
+    const service = new MotorcycleService();
+    const result = await service.create(input);
+
+    // Assert
+    expect(result).to.be.deep.equal(output);
+  });
+});
