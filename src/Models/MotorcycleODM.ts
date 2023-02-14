924b7353ab76d55a96584bcf097d71dcc958b25f
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:29:06 -0300

    :sparkles: feat: Implementa MotorcycleODM

---
+++ src/Models/MotorcycleODM.ts
@@ -0,0 +1,20 @@
+import { Schema } from 'mongoose';
+import AbstractODM from './AbstractODM';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+
+class MotorcycleODM extends AbstractODM<IMotorcycle> {
+  constructor() {
+    const schema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean, required: false },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+    });
+    super(schema, 'Motorcycle');
+  }
+}
+
+export default MotorcycleODM;
