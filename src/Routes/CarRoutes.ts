7a13375b1176f81272b0b5e4eb5da3ab6f6de998
Guilherme Garcia <garciaguig@gmail.com>
2023-02-13 21:25:54 -0300

    :sparkles: feat: Adiciona o endpoint GET /cars

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -8,4 +8,9 @@ routes.post(
   (req, res, next) => new CarController(req, res, next).create(),
 );
 
+routes.get(
+  '/',
+  (req, res, next) => new CarController(req, res, next).getAll(),
+);
+
 export default routes;
