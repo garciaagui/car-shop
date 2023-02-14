2e9eb5f6667a30e034f5dde005d13439d5498146
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 09:59:37 -0300

    :test_tube: test: Adiciona os testes para os cenários de listagem de carro por id

--- tests/unit/Services/CarService.test.ts
+++ tests/unit/Services/CarService.test.ts
@@ -94,4 +94,77 @@ describe('Testes de unidade do Service de Car', function () {
       expect(result).to.be.deep.equal(output);
     });
   });
+
+  describe('Listagem de carros por ID', function () {
+    it('Retorna com SUCESSO o carro cujo id foi passado na URL', async function () {
+      // Arrange
+      const id = '63320b77aa12f0db4f210afe';
+  
+      const input: ICar = {
+        id,
+        model: 'Marea',
+        year: 2002,
+        color: 'Black',
+        buyValue: 15990,
+        doorsQty: 4,
+        seatsQty: 5,
+        status: true,
+      };
+
+      const { model, year, color, status, buyValue, doorsQty, seatsQty } = input;
+
+      const output = new Car(
+        { id,
+          model,
+          year,
+          color,
+          buyValue,
+          doorsQty,
+          seatsQty,
+          status,
+        },
+      );
+  
+      sinon.stub(Model, 'findOne').resolves(output);
+  
+      // Act
+      const service = new CarService();
+      const result = await service.getById(id);
+  
+      // Assert
+      expect(result).to.be.deep.equal(output);
+    });
+
+    it('Retorna uma exceção quando nenhum carro for encontrado', async function () {
+      // Arrange
+      const id = '63320b77aa12f0db4f210afe';
+  
+      sinon.stub(Model, 'findOne').resolves(null);
+  
+      // Act
+      try {
+        const service = new CarService();
+        await service.getById(id);
+      } catch (error) {
+        // Assert
+        expect((error as Error).message).to.be.equal('Car not found');
+      }
+    });
+  
+    it('Retorna uma exceção quando o id for inválido', async function () {
+      // Arrange
+      const id = 'xxx';
+  
+      sinon.stub(Model, 'findOne').resolves(null);
+  
+      // Act
+      try {
+        const service = new CarService();
+        await service.getById(id);
+      } catch (error) {
+        // Assert
+        expect((error as Error).message).to.be.equal('Invalid mongo id');
+      }
+    });
+  });
 });
