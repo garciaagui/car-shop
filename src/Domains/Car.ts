26c5bc227f101200ce4f64a45953979fc0b97f86
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:22:16 -0300

    :recycle: refactor: Remove os métodos get/set redundantes no domain de Car

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -10,55 +10,7 @@ class Car extends Vehicle {
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
   }
-
-  public setId(id: string) {
-    this.id = id;
-  }
-
-  public getId() {
-    return this.id;
-  }
-
-  public setModel(model: string) {
-    this.model = model;
-  }
-
-  public getModel() {
-    return this.model;
-  }
-
-  public setYear(year: number) {
-    this.year = year;
-  }
-
-  public getYear() {
-    return this.year;
-  }
-
-  public setColor(color: string) {
-    this.color = color;
-  }
-
-  public getColor() {
-    return this.color;
-  }
-
-  public setStatus(status: boolean) {
-    this.status = status;
-  }
-
-  public getStatus() {
-    return this.status;
-  }
-
-  public setBuyValue(buyValue: number) {
-    this.buyValue = buyValue;
-  }
-
-  public getBuyValue() {
-    return this.buyValue;
-  }
-
+  
   public setDoorsQty(doorsQty: number) {
     this.doorsQty = doorsQty;
   }
