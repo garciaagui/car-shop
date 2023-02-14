37121abce758d4d8342dd01c5eb5098807421af4
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 12:20:04 -0300

    :test_tube: test: Ajusta os testes de atualização de carros (Service)

--- tests/unit/Services/updateCar.test.ts
+++ tests/unit/Services/updateCar.test.ts
@@ -52,17 +52,9 @@ describe('Testes de unidade de atualização do Service de Car', function () {
     sinon.stub(Model, 'findOne').resolves(findOutput);
     sinon.stub(Model, 'updateOne').resolves();
 
-    // { 
-    //   acknowledged: true, 
-    //   matchedCount: 1,
-    //   modifiedCount: 1,
-    //   upsertedCount: 0,
-    //   upsertedId: null,
-    // },
-
     // Act
     const service = new CarService();
-    const result = await service.update(id);
+    const result = await service.update(id, updateInput);
 
     // Assert
     expect(result).to.be.deep.equal(updateOutput);
@@ -72,13 +64,24 @@ describe('Testes de unidade de atualização do Service de Car', function () {
     // Arrange
     const id = '63320b77aa12f0db4f210afe';
 
+    const updateInput: ICar = {
+      id,
+      model: 'Santana',
+      year: 1999,
+      color: 'Grey',
+      buyValue: 10500,
+      doorsQty: 4,
+      seatsQty: 5,
+      status: true,
+    };
+
     sinon.stub(Model, 'findOne').resolves(null);
     sinon.stub(Model, 'updateOne').resolves();
 
     // Act
     try {
       const service = new CarService();
-      await service.update(id);
+      await service.update(id, updateInput);
     } catch (error) {
       // Assert
       expect((error as Error).message).to.be.equal('Car not found');
@@ -89,13 +92,24 @@ describe('Testes de unidade de atualização do Service de Car', function () {
     // Arrange
     const id = 'xxx';
 
+    const updateInput: ICar = {
+      id,
+      model: 'Santana',
+      year: 1999,
+      color: 'Grey',
+      buyValue: 10500,
+      doorsQty: 4,
+      seatsQty: 5,
+      status: true,
+    };
+
     sinon.stub(Model, 'findOne').resolves(null);
     sinon.stub(Model, 'updateOne').resolves();
 
     // Act
     try {
       const service = new CarService();
-      await service.update(id);
+      await service.update(id, updateInput);
     } catch (error) {
       // Assert
       expect((error as Error).message).to.be.equal('Invalid mongo id');
