7284950641107425f236a43b31f465542d7bca0e
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 16:34:27 -0300

    :sparkles: feat: Implementa os endpoints GET /motorcycles e GET/motorcycles/:id

--- src/Routes/MotorcycleRoutes.ts
+++ src/Routes/MotorcycleRoutes.ts
@@ -8,4 +8,14 @@ routes.post(
   (req, res, next) => new MotorcycleController(req, res, next).create(),
 );
 
+routes.get(
+  '/',
+  (req, res, next) => new MotorcycleController(req, res, next).getAll(),
+);
+
+routes.get(
+  '/:id',
+  (req, res, next) => new MotorcycleController(req, res, next).getById(),
+);
+
 export default routes;
