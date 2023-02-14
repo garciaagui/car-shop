7c5fc17ece2de7486be5edd5fbecb8e828538e4f
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 17:18:39 -0300

    :sparkles: feat: Implementa o endpoint DELETE /car/:id

--- src/Routes/CarRoutes.ts
+++ src/Routes/CarRoutes.ts
@@ -23,4 +23,9 @@ routes.put(
   (req, res, next) => new CarController(req, res, next).update(),
 );
 
+routes.delete(
+  '/:id',
+  (req, res, next) => new CarController(req, res, next).delete(),
+);
+
 export default routes;
