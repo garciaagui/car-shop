67db1885d392758c34fe62fae6546fe6e99d3fc8
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:21:04 -0300

    :recycle: refactor: Refatora o domain de Car

--- src/Domains/Car.ts
+++ src/Domains/Car.ts
@@ -1,22 +1,12 @@
 import ICar from '../Interfaces/ICar';
+import Vehicle from './Vehicle';
 
-class Car {
-  protected id: string | undefined;
-  protected model: string;
-  protected year: number;
-  protected color: string;
-  protected status?: boolean;
-  protected buyValue: number;
+class Car extends Vehicle {
   private doorsQty: number;
   private seatsQty: number;
 
   constructor(car: ICar) {
-    this.id = car.id;
-    this.model = car.model;
-    this.year = car.year;
-    this.color = car.color;
-    this.status = car.status || false;
-    this.buyValue = car.buyValue;
+    super(car);
     this.doorsQty = car.doorsQty;
     this.seatsQty = car.seatsQty;
   }
