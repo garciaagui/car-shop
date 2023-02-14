63bdc2616f1fc5a7f4a8d3858ce0ff1612dbce55
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 16:32:21 -0300

    :sparkles: feat: Implementa as funções 'getAll' e 'getById' em MotorcycleService

--- src/Services/MotorcycleService.ts
+++ src/Services/MotorcycleService.ts
@@ -1,6 +1,9 @@
+import { isValidObjectId } from 'mongoose';
 import Motorcycle from '../Domains/Motorcycle';
 import IMotorcycle from '../Interfaces/IMotorcycle';
 import MotorcycleODM from '../Models/MotorcycleODM';
+import NotFoundException from '../Exceptions/NotFound';
+import UnprocessableException from '../Exceptions/Unprocessable';
 
 class MotorcycleService {
   private createMotorcycleDomain(motorcycle: IMotorcycle | null): Motorcycle | null {
@@ -23,6 +26,26 @@ class MotorcycleService {
     const motorcycleODM = new MotorcycleODM();
     const newMotorcycle = await motorcycleODM.create(motorcycle);
     return this.createMotorcycleDomain(newMotorcycle);
+  }
+
+  public async getAll() {
+    const motorcycleODM = new MotorcycleODM();
+    const motorcycles = await motorcycleODM.getAll();
+    const motorcyclesArr = motorcycles.map((motorcycle) => this.createMotorcycleDomain(motorcycle));
+    return motorcyclesArr;
+  }
+
+  public async getById(id: string) {
+    if (!isValidObjectId(id)) throw new UnprocessableException('Invalid mongo id');
+
+    const motorcycleODM = new MotorcycleODM();
+    const foundMotorcycle = await motorcycleODM.getById(id);
+
+    if (!foundMotorcycle) {
+      throw new NotFoundException('Motorcycle not found');
+    }
+
+    return this.createMotorcycleDomain(foundMotorcycle);
   }
 }
 
