a2ac5f688649406f53851205f7d738c461574372
Guilherme Garcia <garciaguig@gmail.com>
2023-02-14 13:38:00 -0300

    :sparkles: feat: Implementa 'create' em MotorcycleController

---
+++ src/Controllers/MotorcycleController.ts
@@ -0,0 +1,30 @@
+import { NextFunction, Request, Response } from 'express';
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import MotorcycleService from '../Services/MotorcycleService';
+
+class MotorcycleController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+  private service: MotorcycleService;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+    this.service = new MotorcycleService();
+  }
+
+  public async create() {
+    const motorcycle: IMotorcycle = { ...this.req.body };
+
+    try {
+      const newMotorcycle = await this.service.create(motorcycle);
+      return this.res.status(201).json(newMotorcycle);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+
+export default MotorcycleController;
